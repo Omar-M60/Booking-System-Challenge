f997c5a72ec35ece05aaba6535a446f149f3a0d9
Omar Moussalli <omar.y.mousali@gmail.com>
2025-06-12 01:18:21 +0300

    chore: remove ignored files and add .gitignore

--- dist/models/Events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const eventsSchema = new mongoose_1.Schema({
-    eventID: { type: String, required: true, },
-    name: { type: String, required: true, },
-});
-const Event = (0, mongoose_1.model)("Events", eventsSchema);
-exports.default = Event;
